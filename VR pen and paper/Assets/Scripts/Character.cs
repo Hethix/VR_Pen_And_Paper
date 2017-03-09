4a5e315272d88bb97be088e38dd940ba4732e588
AndersonDamN <a0@l4l.dk>
2017-03-09 09:43:03 +0100

    Variables and methods added
    Added variables; HP, Cooldown, Attack, Dmg and ArmourClass. Note that
    they are all currently public.
    
    Added methods; GetHP(), SetHP(), RollRandom(), PerformAttack(), Deal
    Damage(), Die(), SetCooldown(), CheckCooldown(), OnCollisionEnter()
    
    Also note that the OnCollisionEnter() is currently applied to character
    ... I would suggest moving it to "weapon"

--- VR pen and paper/Assets/Scripts/Character.cs
+++ VR pen and paper/Assets/Scripts/Character.cs
@@ -2,63 +2,81 @@
 using System.Collections;
 
 public class Character : MonoBehaviour {
-
-    public int HP;
-    public int attack; // attack is a modifier for the attack roll
-    public int dmg; // Modifier applied to damage rolls
-    public int weaponDmg; // This is the die size used for damage
-    public int AC; 
+    
+    public sbyte HP; //Obvious
+    public byte attack; // attack is a modifier for the attack roll
+    public byte dmg; // Modifier applied to damage rolls
+    public byte weaponDmg; // This is the die size used for damage
+    public byte AC; //Armour Class
+    public float cooldown; 
     Random random = new Random();
+
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
-    public int GetHP() // HP getter
+    // HP getter
+    public sbyte GetHP() 
     {
         return HP;
     }
 
-    public void SetHP(int _HP) // HP setter
+    // HP setter
+    public void SetHP(sbyte _HP) 
     {
         HP = _HP;
     }
 
-    int RollRandom(int max)
+    //Generate a random number between 1 and max, including both.
+    byte RollRandom(byte max) 
     {
-        return (int)Random.Range(1, max); //Assumes the minimum value to be 1
-        // WHOOPS! Check if the typecasting can roll 1 ... of if it rounds up. 
+        byte temp = (byte)Random.Range(1, max+1);  //Assumes the minimum value to be 1
+        // Debug.Log("Roll " + "d" + max + " = " + temp);                                                                   Print
+        return temp;
     }
 
-    void PerformAttack(Character attacker, Character defender) //Standard attack
+    //Standard attack
+    void PerformAttack(Character defender) 
     {
-        int tempHit = attacker.RollRandom(20);
-        if (tempHit == 20)
+        if (CheckCooldown() == true)
         {
-            DealDamage(attacker, defender, true);
+            byte tempHit = RollRandom(20);
+            if (tempHit == 20)
+            {
+                //Debug.Log("Critical!");                                                                                       Print
+                DealDamage(defender, true);
+            }
+            else if (tempHit + attack >= defender.AC)
+            {
+                //Debug.Log("Hit!");                                                                                            Print
+                DealDamage(defender, false);
+
+            }
+            else
+            {
+                //Debug.Log("miss!");                                                                                           Print
+            }
+            SetCooldown(2);
         }
-        else if (tempHit+attack >= defender.AC)
-        {
-            DealDamage(attacker, defender, false);
-
-        }  
     }
 
-    void DealDamage(Character attacker, Character defender, bool crit)
+    //Apply damage from standard attack
+    void DealDamage(Character defender, bool crit)
     {
-        int tempDmg; 
+        byte tempDmg; 
         if (crit == true) {
-            tempDmg = attacker.RollRandom(weaponDmg) + attacker.RollRandom(weaponDmg) + dmg;
+            tempDmg = (byte)(RollRandom(weaponDmg) + RollRandom(weaponDmg) + dmg);
+            // Debug.Log(tempDmg);                                                                                         Print
         }
         else
         {
-            tempDmg = attacker.RollRandom(weaponDmg) + dmg;
+            tempDmg = (byte)(RollRandom(weaponDmg) + dmg);
+            // Debug.Log("dmg: " + tempDmg);                                                                              Print
         }
         
         if (defender.GetHP() - tempDmg <= 0)
@@ -67,12 +85,36 @@ public class Character : MonoBehaviour {
         }
         else
         {
-            defender.SetHP(defender.GetHP() - tempDmg);
+            defender.SetHP((sbyte)(defender.GetHP() - tempDmg));
         }
     }
 
-    void die()
+    // Remove slain object
+    void Die() 
     {
-
+        Destroy(gameObject);
     }
+
+    //Takes a float of time which is added to the general cooldown (time until net ability can be used)
+    void SetCooldown(float timeInSecs) { 
+        cooldown = Time.time + timeInSecs; 
+    }
+
+    // Returns true/false whether or not the cooldown has run out. 
+    bool CheckCooldown() {  
+        if (cooldown < Time.time)
+            return true;
+        else
+            return false;
+    }
+
+    // Detects collision, and performs attack if other object is labelled Enemy
+    void OnCollisionEnter(Collision target)
+    {
+        Character chara = target.gameObject.GetComponent<Character>();
+        if (target.gameObject.tag.Equals("Enemy") == true) 
+            if (CheckCooldown() == true)
+                PerformAttack(chara);
+    }
+
 }
